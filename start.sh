3d512d556780046b2a2eefacc8802bf5aa4e4dd8
leonkae <leon29kae@gmail.com>
2022-05-12 00:53:25 +0300

    added start file

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ export SECRET_KEY='WREWH9WEHGIOEHWGIOEHGNES'
 # python3 manage.py shell
 python manage.py server
 # python manage.py db init
-# python manage.py db migrate -m "Initial commit"
+# python manage.py db migrate -m "schema"
 # python manage.py db upgrade
+# python manage.py db stamp heads
