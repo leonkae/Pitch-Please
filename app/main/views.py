197ffacd2c392ad502303a9032c04736c3ec3540
leonkae <leon29kae@gmail.com>
2022-05-12 00:49:18 +0300

    modified views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,27 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import main_blueprint as main
+from flask_login import login_required
+from .forms import PitchForm
+from .models import Pitches
+from app import db
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
+@main.route('/pitches')
+@login_required
+def pitches():
+    return render_template('pitches.html')
 
 
+@main.route('/pitches/add', methods=['GET', 'POST'])
+@login_required
+def createPitch():
+    create_pitch=PitchForm()
+    if create_pitch.validate_on_submit():
+        pitch = Pitches(title=create_pitch.title.data, body=create_pitch.body.data, category=create_pitch.category.data)
+        db.session.add(pitch)
+        db.session.commit()
+        return redirect(url_for('main_blueprint.pitches'))
+    return render_template('createpitch.html',create=create_pitch)
