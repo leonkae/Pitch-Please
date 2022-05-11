45f6d4c04285bd0df1970021ea8880ed9fadd77d
leonkae <leon29kae@gmail.com>
2022-05-12 00:54:01 +0300

    added new files and db, updated

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+
+class PitchForm(FlaskForm):
+    title = StringField('enter title')
+    body = TextAreaField('lets hear your pitch')
+    category = SelectField(u'Select Category', choices=[( 'Interview Pitch'), ('Product Pitch'), ( 'Pickup Lines')])
+    submit=SubmitField('create pitch')
+    
