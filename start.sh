ea96483d357c48fee56bc05b1033270ec77dbf3e
leonkae <leon29kae@gmail.com>
2022-05-18 16:19:11 +0300

    changes on ignore file

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@ export SQLALCHEMY_DATABASE_URI='postgresql://jurassic:pass123@localhost/newpitch
 export SECRET_KEY='WREWH9WEHGIOEHWGIOEHGNES'
 
 # python3 manage.py shell
-# python manage.py server
-python manage.py db init
-python manage.py db migrate -m "schema updates for profile"
-python manage.py db upgrade
+python manage.py server
+# python manage.py db init
+# python manage.py db migrate -m "schema updates for profile"
+# python manage.py db upgrade
 # python manage.py db stamp heads
